98192f4fcfbe5e7bc979c9d1683897210b0d9c52
David <david.sterz@iubh-fernstudium.de>
2016-06-18 23:00:57 +0200

    Beispielcode für Insert Statement

--- src/java/Database/DBConnect.java
+++ src/java/Database/DBConnect.java
@@ -3,6 +3,7 @@ package Database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 
 /*
@@ -13,41 +14,62 @@ import java.sql.SQLException;
 * public
 */ 
 
-public class DBConnect 
+public class DBConnect     
+{
+    public static void main(String[] args) 
     {
-        public static void main(String[] args) 
-        {
-            String DBServer = "Localhost";
-            String DBInstance = "dasticket";
-            String DBUser = "Admin";
-            String DBPassword = "Admin123";
-            
-            // Initialisiere Treiber
-             try 
-            {
-                Class.forName("com.mysql.jdbc.Driver").newInstance();
-                System.out.println("Treiber initialisiert");
-            } 
-             catch (Exception ex) 
-            {
-             System.out.println(ex.getMessage());
-            }
-            
-            // Initialisiere Verbindung zur Datenbank
-            Connection conn = null;
-            try 
-            {
-                conn = DriverManager.getConnection("jdbc:mysql://" + DBServer + "/" + DBInstance +
-                       "?user=" + DBUser + "&password=" + DBPassword);
+        String DBServer = "Localhost";
+        String DBInstance = "dasticket";
+        String DBUser = "Admin";
+        String DBPassword = "Admin123";
 
-            System.out.println("DB verbunden");
-            } 
-            catch (SQLException ex) 
-            {
+        // Initialisiere Treiber
+         try 
+        {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            System.out.println("Treiber initialisiert");
+        } 
+         catch (Exception ex) 
+        {
+         System.out.println(ex.getMessage());
+        }
+
+        // Initialisiere Verbindung zur Datenbank
+        Connection conn = null;
+        try 
+        {
+            conn = DriverManager.getConnection("jdbc:mysql://" + DBServer + "/" + DBInstance +
+                   "?user=" + DBUser + "&password=" + DBPassword);
+
+        System.out.println("DB verbunden");
+        } 
+        catch (SQLException ex) 
+        {
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        // Statement Object
+
+            
+        // Insert Statement
+         try 
+         {
+            Statement statement = conn.createStatement();
+
+            // Data insert
+            statement.executeUpdate("INSERT INTO usergroup " + "VALUES (1, 'Admins')");
+
+            System.out.println("Insert Data");
+
+         } 
+         catch (SQLException ex) 
+         {
                 System.out.println("SQLException: " + ex.getMessage());
                 System.out.println("SQLState: " + ex.getSQLState());
                 System.out.println("VendorError: " + ex.getErrorCode());
-            }
-        }
+         }
+        
+    }
 
-       }
+   }
