c2d8fc3aca11d6afaff1452434785581a27e1ace
David <david.sterz@iubh-fernstudium.de>
2016-06-25 01:23:44 +0200

    Erstellung Settings Klasse

--- src/java/Internal/Settings.java
+++ src/java/Internal/Settings.java
@@ -1,14 +1,70 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Internal;
 
-/**
- *
- * @author Administrator
- */
-public class Settings {
+/*
+* Settings
+* Getter und Setter für Settings Attribute 
+*
+* <Klassenvariablen>
+* id = Primary Key
+* module = Bereich
+* param = Einstellung
+* value = Wert
+*
+* <Sichtbarkeit>
+* public
+*/ 
+
+public class Settings 
+
+{
+    private int id;
+    private char module;
+    private char param;
+    private char value;
+    
+    // Getter Methoden
+     public int getId() 
+    {
+        return id;
+    }
+    
+    public char getModule() 
+    {
+        return module;
+    }
+    
+    public char getParam() 
+    {
+        return param;
+    }
+    
+    public char getValue() 
+    {
+        return value;
+    }
+    
+
+    // Setter Methoden
+	
+    public void setId(int newId) 
+    {
+        id = newId;
+    }
+    
+    public void setModule(char newModule) 
+    {
+        module = newModule;
+    }
+    
+    public void setParam (char newParam)
+    {
+        param = newParam;
+    }
+    
+    public void setValue (char newValue)
+    {
+        param = newValue;
+    }
     
 }
