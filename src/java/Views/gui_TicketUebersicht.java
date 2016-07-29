713e9f3ce50f23cf54f165aeb1c3427fe6ac144f
David <david.sterz@iubh-fernstudium.de>
2016-07-29 23:17:51 +0200

    Erste Managed Properties eingefügt

---
+++ src/java/Views/gui_TicketUebersicht.java
@@ -0,0 +1,211 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Views;
+
+import Database.DBController;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.application.FacesMessage;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.time.LocalDateTime;
+import javax.faces.model.SelectItem;
+import javax.annotation.PostConstruct;
+
+/*
+* gui_TicketUebersicht
+* Funktionalität für Ticketsuche und Übersicht
+*
+* <Klassenvariablen>
+    String Priority = Ticketpriorität
+    String Category = Kategorie
+    String Course = Ausgewählter Kurs
+    String Title = Titel des Ticket
+    String Comment = Notizen
+    String NewComment = Neue Notizen
+    String Username = Aktueller Benutzername
+    String Status = Status des Ticket
+    Date creationDate = Erstellungsdatum des Tickets
+    Integer ReportedTime = Berechnete Zeit am Ticket
+    List CourseList = Kursliste aus DB
+    List CategoryList = Kategorielist aus DB
+    List PriorityList = Prioritätenliste aus DB
+    List StatusList = Statusliste aus DB
+*
+* <Sichtbarkeit>
+* public
+*/ 
+
+// Managed Bean 
+@ManagedBean
+@SessionScoped
+
+public class gui_TicketUebersicht 
+{
+    String Priority;
+    String Category;
+    String Course;
+    String Title;
+    //String Username = "Admin";
+    String Status;
+    String CreationDate;
+    List<SelectItem> CourseList;
+    List<SelectItem> CategoryList;
+    List<SelectItem> PriorityList;
+    List<SelectItem> StatusList;
+    
+    // Test!!!
+    Integer ID = 18;
+    String Username = "Admin";
+    
+    // Initialisieren und laden der Select Einträge
+    @PostConstruct
+    public void Init()        
+    {
+        
+        String Result;
+        Integer i;
+           
+        // Prioritäten laden
+        PriorityList = new ArrayList<SelectItem>();
+        i = 1;
+        Result = "Start";
+        while (Result != null ) 
+        {   
+          Result = DBController.GetData("priority", "name", "where id= '" + i + "'");
+          if (Result != null)
+          {
+          PriorityList.add(new SelectItem(Result, Result));
+          }
+          i++;
+        }
+        
+        // Kategorie laden
+        CategoryList = new ArrayList<SelectItem>();
+        i = 1;
+        Result = "Start";
+        while (Result != null ) 
+        {   
+          Result = DBController.GetData("category", "name", "where id= '" + i + "'");
+          if (Result != null)
+          {
+          CategoryList.add(new SelectItem(Result, Result));
+          }
+          i++;
+        }
+                
+        // Kurse laden
+        CourseList = new ArrayList<SelectItem>();
+        i = 1;
+        Result = "Start";
+        while (Result != null ) 
+        {   
+          Result = DBController.GetData("courses", "name", "where id= '" + i + "'");
+          if (Result != null)
+          {
+          CourseList.add(new SelectItem(Result, Result));
+          }
+        i++;
+        }
+        
+        // Status laden
+        StatusList = new ArrayList<SelectItem>();
+        i = 1;
+        Result = "Start";
+        while (Result != null ) 
+        {   
+          Result = DBController.GetData("state", "name", "where id= '" + i + "'");
+          if (Result != null)
+          {
+          StatusList.add(new SelectItem(Result, Result));
+          }
+          i++;
+        }
+    }
+    
+    
+     // Getter Methoden
+    public List<SelectItem> getCourseList()
+    {
+    return CourseList;
+    }
+    public List<SelectItem> getPriorityList()
+    {
+    return PriorityList;
+    }
+    public List<SelectItem> getCategoryList()
+    {
+    return CategoryList;
+    }
+    public List<SelectItem> getStatusList()
+    {
+    return StatusList;
+    }
+     public String getPriority() 
+    {
+	return Priority;
+    }
+    public String getCategory() 
+    {
+	return Category;
+    }
+    public String getCourse() 
+    {
+	return Course;
+    }
+    public String getTitle() 
+    {
+	return Title;
+    }
+    public String getUsername() 
+    {
+	return Username;
+    }
+    public String getStatus() 
+    {
+	return Status;
+    }
+    public String getCreationDate() 
+    {
+	return CreationDate;
+    }
+    
+    // Setter Methoden
+    public void setPriority(String Priority) 
+    {
+        this.Priority = Priority;
+    }
+    public void setCategory(String Category) 
+    {
+        this.Category = Category;
+    }
+    public void setCourse(String Course) 
+    {
+        this.Course = Course;
+    }
+    public void setTitle(String Title) 
+    {
+        this.Title = Title;
+    }
+    public void setUsername(String Username) 
+    {
+        this.Username = Username;
+    }
+    public void setStatus(String Status) 
+    {
+        this.Status = Status;
+    }
+        public void creationDate(String CreationDate) 
+    {
+        this.CreationDate = CreationDate;
+    }
+
+}
+
+    
+
